fe05a1be1db6f18c80100f02eceaa50d9d7a34ef
yearin122 <rabbit2122@daum.net>
2024-04-11 14:47:25 +0900

    mainactivity

--- app/src/main/java/com/example/financiallog/HomeMain.kt
+++ app/src/main/java/com/example/financiallog/HomeMain.kt
@@ -1,5 +1,6 @@
 package com.example.financiallog
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.CalendarView
@@ -20,6 +21,7 @@ class HomeMain: AppCompatActivity() {
     lateinit var re_expend: RecyclerView;
     lateinit var re_income : RecyclerView;
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,15 +40,15 @@ class HomeMain: AppCompatActivity() {
 
         // 지출 내역 화면에 보여주기
         re_expend = findViewById<RecyclerView>(R.id.expend_re)
-        re_expend.layoutManager = LinearLayoutManager(this)
+       // re_expend.layoutManager = LinearLayoutManager(this)
 
         // 수입 내역 화면에 보여주기
         re_income = findViewById<RecyclerView>(R.id.income_re)
-        re_income.layoutManager = LinearLayoutManager(this)
+        //re_income.layoutManager = LinearLayoutManager(this)
 
 
         // 하단바 버튼
-        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_view)
         bottomNavigation.setOnItemSelectedListener { item ->
             when(item.itemId){
                 R.id.home -> {
@@ -58,20 +60,9 @@ class HomeMain: AppCompatActivity() {
                     Toast.makeText(applicationContext, "financial", Toast.LENGTH_SHORT).show()
                 }
                 R.id.add -> {
-                    when(item.itemId){
-                        R.id.add_income -> {
-                            val intent = Intent(this, IncomeAct::class.java)
-                            startActivity(intent)
-                        }
-                        R.id.add_expend -> {
-                            val intent = Intent(this, ExpendAct::class.java)
-                            startActivity(intent)
-                        }
-                        R.id.add_diary -> {
-                            val intent = Intent(this, DiaryWriteAct::class.java)
-                            startActivity(intent)
-                        }
-                    }
+                    val intent = Intent(this, IncomeAct::class.java)
+                    startActivity(intent)
+
                 }
                 R.id.diary -> {
                     val intent = Intent(this, DiarySns::class.java)
